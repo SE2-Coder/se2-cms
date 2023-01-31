8198b52bd784745a8e58df4bad85f0f854bfb534
SE2-Coder <criatiam.gutierrez@udea.edu.co>
2023-01-31 08:48:26 -0500

    aready css absolute path

--- log/logsignUp.php
+++ log/logsignUp.php
@@ -7,12 +7,12 @@ if (isset($_POST['send'])){
     $pw=password_hash($pwd, PASSWORD_DEFAULT, [15]);
 
     try{
-        require ('conn.php');
+        include ('conn.php');
         echo 'conexion exitosa';
         $bbdd->exec("SET CHARACTER SET utf8");
         $usr="INSERT INTO users (name, pwd) VALUES (:usr, :password_usr)";
         $result=$bbdd->prepare($usr);
-        $result->execute(array(':usr'=>$user, ':password_usr'=>$pwd));
+        $result->execute(array(':usr'=>$user, ':password_usr'=>$pw));
         $result->closeCursor();
 
         header('location:../login.html');
