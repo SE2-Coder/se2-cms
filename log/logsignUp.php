26ff22b43b83de0844af2e06290e0afa1c65900a
SE2-Coder <criatiam.gutierrez@udea.edu.co>
2023-01-30 21:31:22 -0500

    ad log to signUp.php

---
+++ log/logsignUp.php
@@ -0,0 +1,26 @@
+<?php
+
+if (isset($_POST['send'])){
+    $user=$_POST['usr'];
+    $pwd=$_POST['password_usr'];
+
+    $pw=password_hash($pwd, PASSWORD_DEFAULT, [15]);
+
+    try{
+        require ('conn.php');
+        echo 'conexion exitosa';
+        $bbdd->exec("SET CHARACTER SET utf8");
+        $usr="INSERT INTO users (name, pwd) VALUES (:usr, :password_usr)";
+        $result=$bbdd->prepare($usr);
+        $result->execute(array(':usr'=>$user, ':password_usr'=>$pwd));
+        $result->closeCursor();
+
+        header('location:../login.html');
+
+    }catch (Exception $e){
+
+        die('Error: ' . $e->getMessage());
+
+    }
+}
+$bbdd=null;
